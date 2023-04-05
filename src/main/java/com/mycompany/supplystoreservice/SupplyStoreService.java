020cd0c4a9a6af198475b5349784a82da5fa1f27
LucasBoni29 <santosboni2002@gmail.com>
2023-04-04 21:57:05 -0300

    Projeto criado, 04/04/2023

---
+++ src/main/java/com/mycompany/supplystoreservice/SupplyStoreService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.supplystoreservice;
+
+/**
+ *
+ * @author lucas.bsantos59
+ */
+public class SupplyStoreService {
+
+    public static void main(String[] args) {
+       ManutencaoClientes manutencaoClientes = new ManutencaoClientes();
+       manutencaoClientes.setVisible(true);
+    }
+}
---
+++ src/main/java/com/mycompany/supplystoreservice/Validador.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.supplystoreservice;
+
+import java.util.ArrayList;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author lucas.bsantos59
+ */
+public class Validador {
+    
+   public static ArrayList<String> logs = new ArrayList<>();
+
+    public static ArrayList<String> getLogs() {
+        return logs;
+    }
+    
+    public static void ValidarCamposPesquisa(JTextField txtNome, JTextField txtCpf) throws Exception{
+        
+        if(txtNome.getText().isEmpty() && txtCpf.getText().isEmpty()){
+            logs.add("Preencha pelo menos um dos campos");
+        }
+        
+        if(txtCpf.getText().length() < 11){
+            logs.add("Preencha todo o cpf!");
+        }
+        
+    }
+   
+}
