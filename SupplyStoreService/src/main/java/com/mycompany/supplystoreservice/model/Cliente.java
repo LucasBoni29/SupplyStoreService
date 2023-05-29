c574a24614e064ca941190f844341ba344ca07e8
JoseGuilhermeCA <54550498+JoseGuilhermeCA@users.noreply.github.com>
2023-05-29 12:51:39 -0300

    Add files via upload
    TELA ANALITICA

---
+++ SupplyStoreService/src/main/java/com/mycompany/supplystoreservice/model/Cliente.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.supplystoreservice.model;
+
+/**
+ *
+ * @author lucas.bsantos59
+ */
+public class Cliente {
+    
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private Integer telefone;
+    private String email;
+    private String sexo;
+    private String estadoCivil;
+    private Integer dataNascimento;
+    
+    public Cliente(){
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Integer getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Integer telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public Integer getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Integer dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+    
+    
+}
